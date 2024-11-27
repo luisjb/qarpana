8496c3f1d1b2779be81b4c0fac6a75f1bad0decd
luisjb <bustamante_706@hotmail.com>
2024-11-27 11:15:01 -0300

    b

--- gestion-riego-frontend/src/components/Login.js
+++ gestion-riego-frontend/src/components/Login.js
@@ -23,7 +23,7 @@ function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('https://qarpana.com.ar/api/auth/login', {
+            const response = await fetch('https://api.qarpana.com.ar/api/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ nombre_usuario, contraseña }),
