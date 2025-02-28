9be1cfe47b1074bfd960e3a5837d0f7c3380f7a9
luisjb <bustamante_706@hotmail.com>
2025-02-28 18:09:12 -0300

    version2.0 cors

--- gestion-riego-backend/server.js
+++ gestion-riego-backend/server.js
@@ -20,7 +20,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN || 'https://qarpana.com.ar',
+
+    //origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
